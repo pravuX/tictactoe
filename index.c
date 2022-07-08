597dfb587e1dde8f3dbe06e914e79cc57bf5f302
Prabesh Subedi <prabesh.sb@gmail.com>
2022-07-08 08:05:49 +0545

    change val to grid

--- index.c
+++ index.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 #define SIZE 3
 
-void show_grid(char vals[SIZE][SIZE]) {
+void show_grid(char grid[SIZE][SIZE]) {
     int i, j;
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++)
-            printf("%-3c", vals[i][j]);
+            printf("%-3c", grid[i][j]);
         putchar('\n');
     }
 }
-void update_grid(char vals[3][3], int pos_x, int pos_y, char val) {
-    vals[pos_x][pos_y] = val;
+void update_grid(char grid[SIZE][SIZE], int pos_x, int pos_y, char val) {
+    grid[pos_x][pos_y] = val;
 }
 
 int main()
 {
     int x, y;
     char c;
-    char vals[SIZE][SIZE] = {
+    char grid[SIZE][SIZE] = {
                         {'X', 'X', 'O'},
                         {'X', 'X', 'O'},
                         {'O', 'O', 'X'} };
 
     // testing update_grid function;
-    show_grid(vals);
+    show_grid(grid);
     puts("Enter value and position to change in val, pos_x, pos_y format:");
     scanf("%c%d%d", &c, &x, &y);
-    update_grid(vals, x, y, c);
+    update_grid(grid, x, y, c);
     putchar('\n');
-    show_grid(vals);
+    show_grid(grid);
 
     return 0;
 }
